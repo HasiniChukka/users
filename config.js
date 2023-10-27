3a245c38462b893fb12549b4b696c7a95ce6c810
HasiniChukka <127223503+HasiniChukka@users.noreply.github.com>
2023-10-27 18:10:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      host: "localhost",
+      user: "root",
+      password: "@hasini2003",
+      database: "local_jobs",
+      connectTimeout: 60000
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
